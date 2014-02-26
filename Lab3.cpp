e2adecb507a37136554d7af2526958a00c8e1f42
mercviper <mercviper@gmail.com>
2014-02-26 12:25:50 -0600

    Update and rename Lab5_1.cpp to Lab3.cpp
    Moved all structure definitions to the Student.h file

---
+++ Lab3.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include <iostream>;
+#include <string>;
+#include "Student.h";
+
+int main(){
+Student bob = getStudent();
+printStudent(bob);
+char ch;
+cin >> ch;
+return 0;
+}
