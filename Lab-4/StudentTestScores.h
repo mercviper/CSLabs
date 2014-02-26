2955d913b4adfbb241e60967f4c6f16b5a1730f7
mercviper <mercviper@gmail.com>
2014-02-26 16:25:32 -0600

    Create StudentTestScores.h
    Default .h file given in Lab 4 assignment.

---
+++ Lab-4/StudentTestScores.h
@@ -0,0 +1,26 @@
+#ifndef STUDENTTESTSCORES_H
+#define STUDENTTESTSCORES_H
+#include <string>
+
+using namespace std;
+
+ class StudentTestScores{
+    private:
+	string studentName;
+	double *testScores;
+	int numTestScores;
+   public:
+	StudentTestScores(string name = "", int numScores = 0);	// class constructor	StudentTestScores(const StudentTestScores &other);      // class copy constructor
+	~StudentTestScores();  				// class destructor
+//  *** ADD a display member function that displays a student's test scores in the // following format:
+//  student Name <tab> number of test scores <tab> test scores separated by tabs <endl>
+								
+	
+//  *** ADD accessors & mutators HERE	
+
+	
+	// void operator=(const StudentTestScores   &other);
+
+// *** Stream I/O operations here
+};
+#endif 
