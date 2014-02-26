208ce570e1bc8b90b2624ab9b2d6d75f891cd4c8
mercviper <mercviper@gmail.com>
2014-02-26 16:57:11 -0600

    Update StudentTestScores.cpp

--- Lab-4/StudentTestScores.cpp
+++ Lab-4/StudentTestScores.cpp
@@ -1,7 +1,28 @@
-#include <string>
-#include <iostream>
-using namespace std
-
-int main(){
-  return 0;
+StudentTestScores::StudentTestScores(string name, int numScores){
+  studentName=name;
+  numTestScores=numScores;
 }
+
+StudentTestScores::StudentTestScores(const StudentTestScores &other){
+  studentName=other.studentName;
+  testScores=other.testScores;
+  numTestScores=other.numTestScores;
+}
+
+StudentTestScores::~StudentTestScores(){
+  delete [] testScores;
+}
+
+StudentTestScores::Display(){
+  cout << studentName << "\t" << numTestScores;
+  for(int i=0; i<numTestScores; i++){
+    cout << "\t" << testScores[i];
+  }
+  cout << endl;
+}
+
+/*StudentTestScores::operator=(const StudentTestScores &other){
+  studentName=other.studentName;
+  testScores=other.testScores;
+  numTestScores=other.numTestScores;
+}*/
