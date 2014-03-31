ca78f1ba99eb655c0371fecd39224734e5cf59f9
mercviper <mercviper@gmail.com>
2014-03-31 11:34:18 -0500

    Update test.cpp

--- Lab-4/test.cpp
+++ Lab-4/test.cpp
@@ -5,19 +5,20 @@ using namespace std;
 #include "StudentTestScores.h"
 
 int main(){
-	cout << "CREATE new object x, name = Joe numTestScores = 4" << endl << endl;
-	StudentTestScores x("joe", 4);
-	cout << "READ standard input for values for test scores of joe, and rename joe" << endl << endl;
+	int startingNumTestScores=4;
+	cout << "CREATE new object x, name = Joe numTestScores = "<< startingNumTestScores << endl << endl;
+	StudentTestScores x("Joe", startingNumTestScores);
+	cout << "READ standard input for values for test scores of object y, and rename object y" << endl << endl;
 	cin >> x;
-	cout << endl << "CREATE new object y with name = Chris and default numTestScores" << endl << endl;
+	cout << "CREATE new object y with name = Chris and default numTestScores" << endl << endl;
 	StudentTestScores y("Chris");
 	cout << "OUTPUT object y via standard output operators" << endl << endl;
 	cout << y;
-	cout << endl << "ASSIGN to object y object x using assignment operator" << endl << endl;
+	cout << "ASSIGN to object y object x using assignment operator" << endl << endl;
 	y=x;
-	cout << "OUTPUT object x via display function." << endl << endl;
+	cout << "DISPLAY object x via display function." << endl << endl;
 	x.Display();
-	cout << endl << "ADD a test score of 79 to object x" << endl << endl;
+	cout << "ADD a test score of 79 to object x" << endl << endl;
 	x.addTestScore(79);
 	cout << "OUTPUT final values of object x followed by object y" << endl << endl;
 	cout << x;
