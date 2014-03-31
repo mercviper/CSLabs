fbb6a5353ef25d1ed4e6cf526564458067a83a7d
mercviper <mercviper@gmail.com>
2014-03-31 11:34:32 -0500

    Update StudentTestScores.cpp

--- Lab-4/StudentTestScores.cpp
+++ Lab-4/StudentTestScores.cpp
@@ -12,7 +12,7 @@ StudentTestScores::StudentTestScores(const StudentTestScores &other){
 }
 
 StudentTestScores::~StudentTestScores(){
-  delete[] testScores;
+	delete[] testScores;
 }
 
 void StudentTestScores::Display(){
@@ -20,7 +20,7 @@ void StudentTestScores::Display(){
   for(int i=0; i<numTestScores; i++){
     cout << "\t" << testScores[i];
   }
-  cout << endl;
+  cout << endl << endl;
 }
 
 void StudentTestScores::operator=(const StudentTestScores &other){
@@ -39,10 +39,54 @@ ostream& operator<< (ostream &out, const StudentTestScores &Student)
 	out << "Test Scores: ";
 	for (int i = 0; i < Student.numTestScores; i++) 
 		out << Student.testScores[i] << "\t";
-	out << endl;
+	out << endl << endl;
 	return out;
 }
 
 istream& operator>> (istream &in, StudentTestScores &Student)
 {
-	cout << "Enter student's name 
+	cout << "Enter student's name and test scores." << endl;
+	in >> Student.studentName; 		
+	for (int i=0; i < Student.numTestScores; i++)
+		cin >> Student.testScores[i];
+	return in;
+}										
+
+
+//accessors
+string StudentTestScores::getStudentName()//returns studentName
+{
+  return studentName;
+}
+
+double * StudentTestScores::getTestScores() //returns testScores
+{
+  return testScores;
+}
+
+int StudentTestScores::getNumTestScores() // returns numTestScores
+{
+  return numTestScores;
+}
+
+//mutators	
+void StudentTestScores::setStudentName(string NameofStudent){ //Sets the student's name to the passed string
+  studentName = NameofStudent;
+}
+
+void StudentTestScores::addTestScore(double NewScore){ //Adds the passed int as a new test score and increments numTestScores by 1
+ 
+  numTestScores++;
+  double * Scores = new double[numTestScores];
+  double * tmp=testScores;
+  for(int i=0; i<numTestScores-1; i++)
+  {
+  	Scores[i]=testScores[i];
+  }
+  delete[] tmp;
+  
+  Scores[numTestScores-1] = NewScore;
+  
+  testScores = Scores; 
+  
+}
