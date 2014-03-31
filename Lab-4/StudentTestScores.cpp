9967a370c8dfa58e86a071c54a4f73f6de8f06b5
mercviper <mercviper@gmail.com>
2014-03-31 11:26:07 -0500

    Update StudentTestScores.cpp

--- Lab-4/StudentTestScores.cpp
+++ Lab-4/StudentTestScores.cpp
@@ -12,6 +12,7 @@ StudentTestScores::StudentTestScores(const StudentTestScores &other){
 }
 
 StudentTestScores::~StudentTestScores(){
+  delete[] testScores;
 }
 
 void StudentTestScores::Display(){
@@ -44,48 +45,4 @@ ostream& operator<< (ostream &out, const StudentTestScores &Student)
 
 istream& operator>> (istream &in, StudentTestScores &Student)
 {
-	cout << "Enter student's name and test scores." << endl;
-	in >> Student.studentName; 		
-	for (int i=0; i < Student.numTestScores; i++)
-		cin >> Student.testScores[i];
-	return in;
-}										
-
-
-//accessors
-string StudentTestScores::getStudentName()//returns studentName
-{
-  return studentName;
-}
-
-double * StudentTestScores::getTestScores() //returns testScores
-{
-  return testScores;
-}
-
-int StudentTestScores::getNumTestScores() // returns numTestScores
-{
-  return numTestScores;
-}
-
-//mutators	
-void StudentTestScores::setStudentName(string NameofStudent){ //Sets the student's name to the passed string
-  studentName = NameofStudent;
-}
-
-void StudentTestScores::addTestScore(double NewScore){ //Adds the passed int as a new test score and increments numTestScores by 1
- 
-  numTestScores++;
-  double * Scores = new double[numTestScores];
-  double * tmp=testScores;
-  for(int i=0; i<numTestScores-1; i++)
-  {
-  	Scores[i]=testScores[i];
-  }
-  delete[] tmp;
-  
-  Scores[numTestScores-1] = NewScore;
-  
-  testScores = Scores; 
-  
-}
+	cout << "Enter student's name 
