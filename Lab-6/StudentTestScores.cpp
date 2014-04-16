ca00f11b8668ca261bfdaf11bc38b106130c5a09
mercviper <mercviper@gmail.com>
2014-04-16 09:55:48 -0500

    Update StudentTestScores.cpp

--- Lab-6/StudentTestScores.cpp
+++ Lab-6/StudentTestScores.cpp
@@ -5,6 +5,7 @@
 StudentTestScores::StudentTestScores(const StudentTestScores &other){
   studentName=other.studentName;
   numTestScores=other.numTestScores;
+  letterGrade=other.letterGrade;
   testScores = new double[numTestScores];
   for(int i=0; i<other.numTestScores; i++){
 	  testScores[i]=other.testScores[i];
@@ -29,42 +30,48 @@ double StudentTestScores::averageTestScore(){
 	
 	avg = sum/numTestScores;
 	
-	letterGrade='F';
-	if(avg>60){
-		letterGrade='D';
-	}
-	if(avg>70){
-		letterGrade='C';
-	}
-	if(avg>80){
-		letterGrade='B';
-	}
-	if(avg>90){
-		letterGrade='A';
-	}
-	
 	return avg;
 }
 
 void StudentTestScores::Display(){
+	double avg=averageTestScore();
   cout << studentName << "\t" << numTestScores;
   for(int i=0; i<numTestScores; i++){
     cout << "\t" << testScores[i];
   }
-  cout << '\t' << averageTestScore() << '\t' <<  letterGrade << endl;
+	if(numTestScores==0){
+		letterGrade = 'U';
+	}
+  
+	letterGrade='F';
+	if(avg>=60){
+		letterGrade='D';
+	}
+	if(avg>=70){
+		letterGrade='C';
+	}
+	if(avg>=80){
+		letterGrade='B';
+	}
+	if(avg>=90){
+		letterGrade='A';
+	}
+
+  cout << '\t' << avg << '\t' <<  letterGrade << endl;
 }
 
 void StudentTestScores::operator=(const StudentTestScores &other){
   delete[] testScores;
   studentName=other.studentName;
   numTestScores=other.numTestScores;
+  letterGrade=other.letterGrade;
   testScores = new double[numTestScores];
   for(int i=0; i<other.numTestScores; i++){
 	  testScores[i]=other.testScores[i];
   }
 }
 
-ofstream& operator<< (ofstream &out, const StudentTestScores &Student) //look at this in the morning
+ostream& operator<< (ostream &out, const StudentTestScores &Student) //look at this in the morning
 {
 	out << "Name: " << Student.studentName << endl;
 	out << "Num Test Scores:" << Student.numTestScores << endl;
@@ -75,50 +82,23 @@ ofstream& operator<< (ofstream &out, const StudentTestScores &Student) //look at
 	return out;
 }
 
-ifstream& operator>> (ifstream &in, StudentTestScores &Student)
+istream& operator>> (istream &in, StudentTestScores &Student)
 {
 //o	First line data = #students
 //o	Create StudentTestScores array Scores of #students
 //o	For each i < #students, read a line from the fstream into Scores[i]
 	char name[80];
 	double score;
-	in >> name;
-	in.get();
+ 	in.getline(name, 80, '\t' );
+	Student.studentName=name;
 	in >> Student.numTestScores;
-	in.get();
-	Student.testScores=new double[numTestScores];
+	Student.testScores=new double[Student.numTestScores];
 	for (int i=0; i < Student.numTestScores; i++){
 		in >> score;
 		Student.testScores[i]=score;
 		//cout << "this is the score : " << score;
 	}
 	return in;
-	/*
-istream &operator >>(istream &inFile , StudentTestScores &student)
- {
- 	cin.ignore();
- 	char line[50];
- 	inFile.getline( line, 50, '\t' );
- 	student.studentName = line;
- 	cout << line;
-
- 	 
- 	inFile >> student.numTestScores;
- 	cout << "\t" << student.numTestScores << "\t";
- 	  
- 	double *temp;
- 	temp = new double[student.numTestScores];
-
- 	for (int i = 0; i < student.numTestScores; i++)
- 	{
- 	  
- 	 inFile >> temp[i] ; 
- 	 cout << temp[i] << '\t';
- 	  
- 	}
- student.testScores = temp;
- 	return inFile;
- }*/
 }										
 
 
