f907b6149529228ca33322e7b3cb5cdfa3c8dfba
mercviper <mercviper@gmail.com>
2014-04-16 09:56:03 -0500

    Update test.cpp

--- Lab-6/test.cpp
+++ Lab-6/test.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #include "StudentTestScores.h"
@@ -13,29 +14,22 @@ int main(){
 //-	Prompt user for input filepath name.
 	cout << "Where is the file located?" << endl;
 	cin >> filePath;
+	cin.get();
 	inFile.open(filePath, ios::in);
 
 	if(!inFile){
-		cout << "Error! Missing File! Please enter a proper filepath." 
+		cout << "Error! Missing File! Please enter a proper filepath.";
 		return 0;
 	}
 	else	{
 		inFile >> numStudents;
 		scores = new StudentTestScores[numStudents];
 		inFile.get();
-
-		cout << "here we input scores from file" << endl;
 		for(int i = 0; i < numStudents; i++){
 			inFile >> scores[i];
 			inFile.get();
 		}
-
-		 
-		cout << "here we have finished inputting scores from file" << endl;
-		cin.get();
 	}
-		cout << "here parse scores from file and generate the report" << endl;
-		cin.get();
 //-	Read file data to program.
 //o	Create fstream variable
 	//fstream variable >> scores;
@@ -43,82 +37,8 @@ int main(){
 //o	For each student in the array, generate an average score to determine the letter grade
 //o	Then output according to instructions.
 	for(int i=0; i<numStudents; i++){
-		scores[i].display();
+		scores[i].Display();
 	}
+	cin.get();
 	return 0;
 }
-
-/*
-istream &operator >>(istream &inFile , StudentTestScores &student)
- {
- 	cin.ignore();
- 	char line[50];
- 	inFile.getline( line, 50, '\t' );
- 	student.studentName = line;
- 	cout << line;
-
- 	 
- 	inFile >> student.numTestScores;
- 	cout << "\t" << student.numTestScores << "\t";
- 	  
- 	double *temp;
- 	temp = new double[student.numTestScores];
-
- 	for (int i = 0; i < student.numTestScores; i++)
- 	{
- 	  
- 	 inFile >> temp[i] ; 
- 	 cout << temp[i] << '\t';
- 	  
- 	}
- student.testScores = temp;
- 	return inFile;
- }
- 
- fstream inFile;
- StudentTestScores *students;
- string file;
- char answer;
-
-
- cout << "Enter Document name(Case sensitive): " ;
- cin >> file;
- file = file + ".txt";
-
-  
- inFile.open(file, ios::in);
-
- if(!inFile)
- {	cout << "Error! Missing File!" << endl << endl;
- 	cin.get();
- 	cout << "Would you like to create it? (y/n):" ;
- 	cin >> answer;
- 	if (answer == 'y')
- 	 inFile.open(file,ios::out);
- 	else if(answer == 'n')
- 	 {cout << "Thanks!" << endl;}
- 	inFile.close();
- 	cin.get();
- 	 
- }
- else
- {
- 	int tempNum;
- 	inFile >> tempNum;
- 	students = new StudentTestScores[tempNum];
-
-  
- 	for(int i = 0; i < tempNum; i++)
- 	{
- 	 inFile >> students[i];
- 	}
-
- 	 
- }
- cin.get();
- cin.get();
-
-
- return 0;
-}
- */
