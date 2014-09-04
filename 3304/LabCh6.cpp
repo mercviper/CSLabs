9bc0bd9e7b58ab2a1c104ded5ab0bbd1c436ad22
mercviper <mercviper@gmail.com>
2014-09-04 18:54:21 -0500

    Update LabCh6.cpp

--- 3304/LabCh6.cpp
+++ 3304/LabCh6.cpp
@@ -1,1 +1,26 @@
-Placeholder text
+#include <iostream>
+using namespace std;
+
+class List{
+public:
+	int myInt;
+	List * next;
+private:
+	List();
+	~List();
+	void Print() const;
+	void Insert(int);
+	void Delete(int);
+};
+
+List::List(){
+	myInt=0;
+	next=NULL;
+}
+
+List::~List(){
+	while(next!=NULL){
+		this->next->Delete(this->next->myInt);
+	}
+	Delete(myInt);
+}
