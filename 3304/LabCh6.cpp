80788a9dc9ae170898df4350941886b8d2082bfa
mercviper <mercviper@gmail.com>
2014-09-08 15:38:10 -0500

    Update LabCh6.cpp

--- 3304/LabCh6.cpp
+++ 3304/LabCh6.cpp
@@ -2,34 +2,87 @@
 using namespace std;
 
 class List{
-public:
+private:
 	int myInt;
 	List * next;
-private:
-	List();
+public:
+	List(int myInt=0, List * next=NULL);
 	~List();
 	void Print() const;
 	void Insert(int);
 	void Delete(int);
 };
 
-List::List(){
-	myInt=0;
-	next=NULL;
+void test();
+
+int main(){
+	test();
+	char ch;
+	cin >> ch;
+	return 0;
+}
+
+void test(){
+	List * Head = new List(2);
+	Head->Insert(2);
+	Head->Insert(2);
+	Head->Insert(15);
+	Head->Insert(7);
+	Head->Insert(15);
+	Head->Insert(5);
+	Head->Insert(5);
+	Head->Print();
+	Head->Delete(5);
+	Head->Delete(5);
+	Head->Delete(5);
+	Head->Delete(7);
+	Head->Print();
+}
+
+List::List(int x, List * y){
+	myInt=x;
+	next=y;
 }
 
 List::~List(){
-	while(next!=NULL){
-		this->next->Delete(this->next->myInt);
-	}
-	Delete(myInt);
+	cout << "Deleting list node " << myInt << endl;
 }
 
 void List::Print() const {
+	cout << this->myInt << endl;
+	if(this->next!=NULL){
+		this->next->Print();
+	}
 }
 
 void List::Insert(int newInt){
+	List * index=this;
+	List * prev=this;
+	List * newList = new List(newInt);
+	while(index->next!=NULL && index->myInt<=newInt){
+		prev=index;
+		index=index->next;
+	}
+	if(index->myInt>newInt){
+		newList->next=index;
+		prev->next=newList;
+	}
+	else
+		index->next=newList;
 }
 
 void List::Delete(int tarInt){
+	List * index=this;
+	List * prev=this;
+	while(index->next!=NULL && index->myInt!=tarInt){
+		prev=index;
+		index=index->next;
+	}
+	if(index->myInt==tarInt){
+		prev->next=index->next;
+		index->next=NULL;
+		delete index;
+	}
+	else
+		cout << "Target Integer not found in list" << endl;
 }
