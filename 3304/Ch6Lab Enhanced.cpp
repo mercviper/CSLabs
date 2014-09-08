3e9d0c7b742d3e2947d7aa88bcc591e0bfce6879
mercviper <mercviper@gmail.com>
2014-09-08 17:57:12 -0500

    Update Ch6Lab Enhanced.cpp

--- 3304/Ch6Lab Enhanced.cpp
+++ 3304/Ch6Lab Enhanced.cpp
@@ -2,15 +2,26 @@
 using namespace std;
 
 class List{
-private:
-	int myInt;
-	List * next;
-public:
-	List(int myInt=0, List * next=NULL);
-	~List();
-	void Print() const;
-	void Insert(int);
-	void Delete(int);
+	private:
+		class ListNode{
+			private:
+				int myInt;
+				ListNode * next;
+			public:
+				ListNode(int i=0, ListNode * n = NULL) {myInt=i; next=n;}
+				~ListNode(){ cout << "Node has been deleted." << endl;}
+				int getMyInt() {return myInt;}
+				void setMyInt(int i) {myInt=i;}
+				ListNode * getNext() {return next;}
+				void setNext(ListNode * n) {next=n;}
+		};
+		ListNode * head;
+	public:
+		List(ListNode * h=NULL){head=h;}
+		~List();
+		void Print() const;
+		void Insert(int);
+		void Delete(int);
 };
 
 void menu();
@@ -21,64 +32,85 @@ int main(){
 }
 
 void menu(){
-	List * Head = NULL;
-	char choice='0';
+	List * intList = new List;
+	int choice=0;
 	cout << "Lab exercise for Chapter 6 Lists" << endl;
-	while(choice!='4'){
+	while(choice!=4){
 		cout << "Please pick from the following options:" << endl;
 		cout << "[1] Print current list" << endl;
 		cout << "[2] Insert integer to list" << endl;
 		cout << "[3] Delete integer from list" << endl;
 		cout << "[4] Exit" << endl;
 		cin >> choice;
+		cout << endl << endl;
 		switch(choice){
-		case '1':
-			if(Head==NULL)
-				cout << "The list is currently empty" << endl;
-			else
-				Head->Print();
+		case 1:
+			intList->Print();
 			break;
-		case '2':
+		case 2:
 			int newInt;
 			cout << "Enter the new integer to be added" << endl;
 			cin >> newInt;
-			if(Head==NULL)
-				Head=new List(newInt);
-			else
-				Head->Insert(newInt);
+			cout << endl << endl;
+			intList->Insert(newInt);
 			break;
-		case '3': //doesn't work to continue program if first element (Head) is deleted because Head variable no longer exists
+		case 3: 
 			int tarInt;
-			cout << "Enter the integer to be deleted" << endl;
+			cout << "Enter the integer to be deleted" << endl ;
 			cin >> tarInt;
-			Head->Delete(tarInt);
+			cout << endl << endl;
+			intList->Delete(tarInt);
 			break;
 		default:
-			cout << "Please enter a valid option." << endl;
+			cout << "Please enter a valid option." << endl << endl;
 		}
 	}
 
 }
 
-List::List(int x, List * y){
-	myInt=x;
-	next=y;
-}
-
 List::~List(){
-	cout << "Deleting list node " << myInt << endl;
 }
 
 void List::Print() const {
-	cout << this->myInt << endl;
-	if(this->next!=NULL){
-		this->next->Print();
+	if(head==NULL)
+		cout << "The list is empty." << endl << endl;
+	else{
+		cout << "The current list is as follows:" << endl << endl;
+		ListNode * index = head;
+		cout << index->getMyInt() << endl;
+		while(index->getNext()!=NULL){
+			index=index->getNext();
+			cout << index->getMyInt() << endl;
+		}
+		cout << endl << endl;
 	}
 }
 
 void List::Insert(int newInt){
-	List * index=this;
-	List * prev=this;
+	if(head==NULL){
+		ListNode * h = new ListNode(newInt,NULL);
+		head=h;
+	}
+	else{
+		ListNode * index = head;
+		ListNode * prev = NULL;
+		ListNode * newListNode = new ListNode(newInt);
+		while(index->getNext()!=NULL && index->getMyInt()<=newInt){
+			prev=index;
+			index=index->getNext();
+		}
+		if(index->getMyInt()>newInt){
+			newListNode->setNext(index);
+			if(prev!=NULL)
+				prev->setNext(newListNode);
+			else
+				head->setNext(newListNode);
+		}
+		else
+			index->setNext(newListNode);
+	}
+/*	List * index=this;
+	List * prev=NULL;
 	List * newList = new List(newInt);
 	while(index->next!=NULL && index->myInt<=newInt){
 		prev=index;
@@ -89,11 +121,11 @@ void List::Insert(int newInt){
 		prev->next=newList;
 	}
 	else
-		index->next=newList;
+		index->next=newList;*/
 }
 
 void List::Delete(int tarInt){
-	List * index=this;
+	/*List * index=this;
 	List * prev=this;
 	while(index->next!=NULL && index->myInt!=tarInt){
 		prev=index;
@@ -105,5 +137,5 @@ void List::Delete(int tarInt){
 		delete index;
 	}
 	else
-		cout << "Target Integer not found in list" << endl;
+		cout << "Target Integer not found in list" << endl << endl;*/
 }
